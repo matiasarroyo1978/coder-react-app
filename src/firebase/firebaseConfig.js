a03cc819c35a4b964553f9e5c23c22eee428407a
Matias Arroyo <matiasarroyo684@gmail.com>
2022-11-29 09:03:23 -0300

    entrega final

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -6,12 +6,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA0_JI_DGvK4vsLyU1RDxUq05LYVnHpcLY",
-  authDomain: "coder-react-37179.firebaseapp.com",
-  projectId: "coder-react-37179",
-  storageBucket: "coder-react-37179.appspot.com",
-  messagingSenderId: "1047115053794",
-  appId: "1:1047115053794:web:0a43309583ed752d217cbb"
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID
 };
 
 // Initialize Firebase
